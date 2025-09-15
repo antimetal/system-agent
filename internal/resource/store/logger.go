92591cab37af63e4f078d062b860945b8ae6228d
Hamzah Qudsi <hamzah@antimetal.com>
2025-09-15 14:30:59 -0400

    fix(resource): properly format strings in badger logger
    The badger logger interface expects that passed in arguments after
    the initial message are used for string formatting and not structured as
    KV fields.
    
    Signed-off-by: Hamzah Qudsi <hamzah@antimetal.com>

--- internal/resource/store/logger.go
+++ internal/resource/store/logger.go
@@ -6,7 +6,11 @@
 
 package store
 
-import "github.com/go-logr/logr"
+import (
+	"fmt"
+
+	"github.com/go-logr/logr"
+)
 
 // badgerLogger wraps a logr.Logger to implement the badger.Logger interface
 type badgerLogger struct {
@@ -20,22 +24,22 @@ func newBadgerLogger(log logr.Logger) *badgerLogger {
 
 // Errorf logs an ERROR level message
 func (l *badgerLogger) Errorf(format string, args ...any) {
-	l.log.Error(nil, format, args...)
+	l.log.Error(nil, fmt.Sprintf(format, args...))
 }
 
 // Warningf logs a WARNING level message
 func (l *badgerLogger) Warningf(format string, args ...any) {
 	// Map warning to info since logr doesn't have a warning level
-	l.log.Info(format, args...)
+	l.log.Info(fmt.Sprintf(format, args...))
 }
 
 // Infof logs an INFO level message
 func (l *badgerLogger) Infof(format string, args ...any) {
-	l.log.Info(format, args...)
+	l.log.Info(fmt.Sprintf(format, args...))
 }
 
 // Debugf logs a DEBUG level message
 func (l *badgerLogger) Debugf(format string, args ...any) {
 	// Use V(1) for debug level logging
-	l.log.V(1).Info(format, args...)
+	l.log.V(1).Info(fmt.Sprintf(format, args...))
 }
